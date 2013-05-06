d1a91a7a691571c30be043a681d003680b8d67a8
Cory Nathe <cnathe@labkey.com>
2013-05-06 14:43:10 +0000

    Fix for Rlabkey package warning messages re: global variables in ifcookie.R

--- Rlabkey/R/ifcookie.R
+++ Rlabkey/R/ifcookie.R
@@ -14,9 +14,12 @@
 # limitations under the License.
 ##
 
+labkey.sessionCookieName = NA;
+labkey.sessionCookieContents = NA;
+
 ifcookie <- function()
 {
-if(exists("labkey.sessionCookieName")) {Cvalue=1; Cname=labkey.sessionCookieName; 
+if(!is.na(labkey.sessionCookieName)) {Cvalue=1; Cname=labkey.sessionCookieName;
 Ccont=labkey.sessionCookieContents} else {Cvalue=0; Cname=NULL; Ccont=NULL}
 
 return(list(Cvalue=Cvalue,Cname=Cname,Ccont=Ccont))
