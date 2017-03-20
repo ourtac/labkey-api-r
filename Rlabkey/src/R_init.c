9b7c7a0fd7ccbc1cd470454a4f91397df7499620
cnathe <cnathe@labkey.com>
2017-03-20 12:51:45 -0500

    Fix for CRAN note: Found no call to: 'R_useDynamicSymbols'

--- Rlabkey/src/R_init.c
+++ Rlabkey/src/R_init.c
@@ -3,12 +3,16 @@
 #include <R_ext/Rdynload.h>
 #include "RJSON_parser.h"
 
+/* .Call calls */
+extern SEXP Rlabkey_listToMatrix(SEXP, SEXP);
+
 R_CallMethodDef callMethods[] = {
 	{ ".JSON_to_R", (DL_FUNC) &JSON_to_R, 1},
 	{ ".parser_new", (DL_FUNC) &parser_new, 0},
 	{ ".parser_add", (DL_FUNC) &parser_new, 3},
 	{ ".parser_finalize", (DL_FUNC) &parser_new, 1},
 	{ ".parser_delete", (DL_FUNC) &parser_new, 1},
+	{ "Rlabkey_listToMatrix", (DL_FUNC) &Rlabkey_listToMatrix, 2},
 	{NULL, NULL, 0}
 };
 
